e385c027a906146d826c4b9f1ddea9bdf430abb2
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-10-22 10:57:49 -0500

    Banner complete

--- amazon/src/components/Home.js
+++ amazon/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../Home.css';
 
 function Home() {
 	return (
 		<div className="home">
 			<img
+				className="home__image"
 				src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Yzk3OWQ3NDUt/Yzk3OWQ3NDUt-ZWY0YzBhZjAt-w1500._CB418661556_.jpg"
 				alt=""
 			/>
