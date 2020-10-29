2fb039c1e7438ae451afb614944ec6c87da6faba
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-10-28 20:14:40 -0500

    About to connect Firebase

--- amazon/src/components/Login.js
+++ amazon/src/components/Login.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import '../Login.css';
 function Login() {
 	return (
-		<div>
-			<h1>Login Page</h1>
+		<div className="login">
+			<img
+				src="https://i.pinimg.com/originals/31/d1/3c/31d13c99ee841869ca44ef54ba956272.png"
+				alt="amazon logo"
+			/>
 		</div>
 	);
 }
