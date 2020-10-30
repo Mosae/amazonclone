fa780130c9706d0f0cad7d491f95b6425ab366a7
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-10-29 19:38:43 -0500

    created sign in form- need styling

--- amazon/src/components/Login.js
+++ amazon/src/components/Login.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../Login.css';
 function Login() {
 	return (
 		<div className="login">
-			<img
-				src="https://i.pinimg.com/originals/31/d1/3c/31d13c99ee841869ca44ef54ba956272.png"
-				alt="amazon logo"
-			/>
+			<Link to="/">
+				<img
+					className="login__logo"
+					src="http://pngimg.com/uploads/amazon/amazon_PNG16.png"
+					alt="amazon logo"
+				/>
+			</Link>
+			<div className="login__container">
+				<h1>Sign In</h1>
+				<form>
+					<h5>E-mail</h5>
+					<input type="text" />
+					<h5>Password</h5>
+					<input type="password" />
+					<button className="sign__in__button ">Sign in</button>
+					<p>
+						By continuing, you agree to Amazon's Conditions of Use and Privacy
+						Notice.
+					</p>
+				</form>
+				<hr />
+				New to Amazon?
+				<button>Create your Amazon account</button>
+			</div>
 		</div>
 	);
 }
