5dc33e2d85aa5a9c809b17d1f3d1461d5bd8ae26
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-11-02 16:59:28 -0600

    Sign in auth works

--- amazon/src/Header.js
+++ amazon/src/Header.js
@@ -4,10 +4,17 @@ import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import { useStateValue } from './components/StateProvider';
+import { auth } from './components/firebase';
 
 function Header() {
-	const [{ basket }] = useStateValue();
-	console.log(basket);
+	const [{ basket, user }] = useStateValue();
+	console.log(user);
+
+	const login = () => {
+		if (user) {
+			auth.signOut();
+		}
+	};
 	return (
 		<nav className="header">
 			{/* logo left - img */}
@@ -27,10 +34,12 @@ function Header() {
 			</div>
 			{/* 3 clickable links */}
 			<div className="header__nav">
-				<Link to="/login" className="header__link">
-					<div className="header__option">
-						<span className="header__optionOne">Hello, Mosae</span>
-						<span className="header__optionTwo">Sign In</span>
+				<Link to={!user && '/login'} className="header__link">
+					<div onClick={login} className="header__option">
+						<span className="header__optionOne">Hello,{user?.email}</span>
+						<span className="header__optionTwo">
+							{user ? 'Sign out' : 'Sign In'}
+						</span>
 					</div>
 				</Link>
 				<Link to="/login" className="header__link">
