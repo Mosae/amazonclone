5a8a6a70445c926facb42e03949949cf4aa1fe8f
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-11-05 23:25:10 -0600

    Added footer with link to github

--- amazon/src/components/Footer.js
+++ amazon/src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import '../Footer.css';
+import github from '../images/GitHub-Mark-32px.png';
 function Footer() {
 	return (
-		<div>
-			<p>Footer</p>
+		<div className="footer__container">
+			<h2>Amazon Clone By: Mosae Litsoane</h2>
+			<a href="https://github.com/Mosae/amazonclone" target="_blank">
+				<img src={github} alt="github logo" />
+			</a>
 		</div>
 	);
 }
